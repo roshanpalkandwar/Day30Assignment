06dedeb632e129f05e1c6043190c4912ce9994aa
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-10-06 22:28:16 +0530

    [Roshan]Cap Invoice Genrator

---
+++ Genrator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Genrator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine("Total fare for normal ride is " + fare);
+
+            InvoiceGenerator invoiceGenerator1 = new InvoiceGenerator(RideType.PREMIUM);
+            double fare1 = invoiceGenerator1.CalculateFare(2.0, 5);
+            Console.WriteLine("Total fare for premium ride is " + fare1);
+        }
+    }
+}
---
+++ Genrator/RideRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genrator
+{
+    public class RideRepository
+    {
+        Dictionary<string, List<Ride>> userRides = null;
+
+        public RideRepository()
+        {
+            userRides = new Dictionary<string, List<Ride>>();
+        }
+
+        public void AddRide(string userId, Ride[] rides)
+        {
+            bool ridelist = this.userRides.ContainsKey(userId);
+            try
+            {
+                if (!ridelist)
+                {
+                    List<Ride> list = new List<Ride>();
+                    list.AddRange(rides);
+                    userRides.Add(userId, list);
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid User ID");
+            }
+        }
+
+        public Ride[] GetRides(string userId)
+        {
+            try
+            {
+                return this.userRides[userId].ToArray();
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invaliod User Id");
+            }
+        }
+    }
+}
